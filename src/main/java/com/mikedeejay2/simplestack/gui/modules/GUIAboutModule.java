4adfdcfba21718c75f44930191a059eb53f4c7fb
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2020-11-15 21:25:58 -0500

    More work on about screen

--- src/main/java/com/mikedeejay2/simplestack/gui/modules/GUIAboutModule.java
+++ src/main/java/com/mikedeejay2/simplestack/gui/modules/GUIAboutModule.java
@@ -10,12 +10,137 @@ import com.mikedeejay2.mikedeejay2lib.util.head.Base64Heads;
 import com.mikedeejay2.mikedeejay2lib.util.item.ItemCreator;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Random;
 
 public class GUIAboutModule extends GUIModule
 {
+    @Override
+    public void onClickedHead(InventoryClickEvent event, GUIContainer gui)
+    {
+        GUILayer clickLayer = gui.getLayer("click");
+        int slot = event.getSlot();
+        int rows = gui.getRows();
+        int cols = gui.getCols();
+        int row = gui.getRowFromSlot(slot);
+        int col = gui.getColFromSlot(slot);
+        int newRow = row;
+        int newCol = col;
+
+        AnimatedGUIItem item = null;
+
+        item = new AnimatedGUIItem(null, false, 0);
+        item.addFrame(ItemCreator.createItem(Material.WHITE_STAINED_GLASS_PANE, 1, GUIContainer.EMPTY_NAME), 1);
+        item.addFrame(ItemCreator.createItem(Material.PINK_STAINED_GLASS_PANE, 1, GUIContainer.EMPTY_NAME), 1);
+        item.addFrame(ItemCreator.createItem(Material.LIGHT_BLUE_STAINED_GLASS_PANE, 1, GUIContainer.EMPTY_NAME), 1);
+        item.addFrame(ItemCreator.createItem(Material.MAGENTA_STAINED_GLASS_PANE, 1, GUIContainer.EMPTY_NAME), 1);
+        item.addFrame(null, 1);
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+
+        item = new AnimatedGUIItem(null, false, 2);
+        item.addFrame(ItemCreator.createItem(Material.PINK_STAINED_GLASS_PANE, 1, GUIContainer.EMPTY_NAME), 1);
+        item.addFrame(ItemCreator.createItem(Material.LIGHT_BLUE_STAINED_GLASS_PANE, 1, GUIContainer.EMPTY_NAME), 1);
+        item.addFrame(ItemCreator.createItem(Material.MAGENTA_STAINED_GLASS_PANE, 1, GUIContainer.EMPTY_NAME), 1);
+        item.addFrame(null, 1);
+        newCol = col - 1;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newRow = row - 1;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newCol = col;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newCol = col + 1;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newRow = row;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newRow = row + 1;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newCol = col;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newCol = col - 1;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+
+        item = new AnimatedGUIItem(null, false, 4);
+        item.addFrame(ItemCreator.createItem(Material.LIGHT_BLUE_STAINED_GLASS_PANE, 1, GUIContainer.EMPTY_NAME), 1);
+        item.addFrame(ItemCreator.createItem(Material.MAGENTA_STAINED_GLASS_PANE, 1, GUIContainer.EMPTY_NAME), 1);
+        item.addFrame(null, 1);
+        newRow = row; newCol = col - 2;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newRow = row - 1;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newCol = col - 1; newRow = row - 2;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newCol = col;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newCol = col + 1;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newCol = col + 2; newRow = row - 1;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newRow = row;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newRow = row + 1;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newRow = row + 2; newCol = col + 1;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newCol = col;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newCol = col - 1;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newCol = col - 2; newRow = row + 1;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+
+        item = new AnimatedGUIItem(null, false, 6);
+        item.addFrame(ItemCreator.createItem(Material.MAGENTA_STAINED_GLASS_PANE, 1, GUIContainer.EMPTY_NAME), 1);
+        item.addFrame(null, 1);
+        newRow = row;
+        newCol = col - 3;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newRow = row - 1;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newRow = row - 2;
+        newCol = col - 2;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newRow = row - 3;
+        newCol = col - 1;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newCol = col;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newCol = col + 1;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newCol = col + 2;
+        newRow = row - 2;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newRow = row - 1;
+        newCol = col + 3;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newRow = row;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newRow = row + 1;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newRow = row + 2;
+        newCol = col + 2;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newRow = row + 3;
+        newCol = col + 1;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newCol = col;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newCol = col - 1;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newCol = col - 2;
+        newRow = row + 2;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+        newCol = col - 3;
+        newRow = row + 1;
+        if(validCheck(rows, cols, newRow, newCol)) clickLayer.setItem(newRow, newCol, item);
+    }
+
+    private boolean validCheck(int rows, int cols, int newRow, int newCol)
+    {
+        return newRow > 0 && newCol > 0 && newRow <= rows && newCol <= cols;
+    }
+
     @Override
     public void onOpenHead(Player player, GUIContainer gui)
     {
@@ -27,6 +152,112 @@ public class GUIAboutModule extends GUIModule
         int cols = gui.getCols();
         genBackground(base);
         genFly(flyLayer);
+        genText(textLayer);
+    }
+
+    private void genText(GUILayer textLayer)
+    {
+        AnimatedGUIItem item1 = new AnimatedGUIItem(null, false, 10);
+        item1.addFrame(ItemCreator.createHeadItem(Base64Heads.WHITE, 1, GUIContainer.EMPTY_NAME), 5);
+        item1.addFrame(-1, 0, MovementType.OVERRIDE_ITEM, true, 5);
+        item1.addFrame(ItemCreator.createHeadItem(Base64Heads.S_WHITE, 1, GUIContainer.EMPTY_NAME), 5);
+        item1.addFrame(ItemCreator.createHeadItem(Base64Heads.S_RED, 1, GUIContainer.EMPTY_NAME), 1);
+        item1.addFrame(ItemCreator.createHeadItem(Base64Heads.S_ORANGE, 1, GUIContainer.EMPTY_NAME), 1);
+        item1.addFrame(ItemCreator.createHeadItem(Base64Heads.S_YELLOW, 1, GUIContainer.EMPTY_NAME), 1);
+
+        AnimatedGUIItem item2 = new AnimatedGUIItem(null, false, 11);
+        item2.addFrame(ItemCreator.createHeadItem(Base64Heads.WHITE, 1, GUIContainer.EMPTY_NAME), 5);
+        item2.addFrame(-1, 0, MovementType.OVERRIDE_ITEM, true, 5);
+        item2.addFrame(ItemCreator.createHeadItem(Base64Heads.I_WHITE, 1, GUIContainer.EMPTY_NAME), 5);
+        item2.addFrame(ItemCreator.createHeadItem(Base64Heads.I_RED, 1, GUIContainer.EMPTY_NAME), 1);
+        item2.addFrame(ItemCreator.createHeadItem(Base64Heads.I_ORANGE, 1, GUIContainer.EMPTY_NAME), 1);
+        item2.addFrame(ItemCreator.createHeadItem(Base64Heads.I_YELLOW, 1, GUIContainer.EMPTY_NAME), 1);
+
+        AnimatedGUIItem item3 = new AnimatedGUIItem(null, false, 12);
+        item3.addFrame(ItemCreator.createHeadItem(Base64Heads.WHITE, 1, GUIContainer.EMPTY_NAME), 5);
+        item3.addFrame(-1, 0, MovementType.OVERRIDE_ITEM, true, 5);
+        item3.addFrame(ItemCreator.createHeadItem(Base64Heads.M_WHITE, 1, GUIContainer.EMPTY_NAME), 5);
+        item3.addFrame(ItemCreator.createHeadItem(Base64Heads.M_RED, 1, GUIContainer.EMPTY_NAME), 1);
+        item3.addFrame(ItemCreator.createHeadItem(Base64Heads.M_ORANGE, 1, GUIContainer.EMPTY_NAME), 1);
+        item3.addFrame(ItemCreator.createHeadItem(Base64Heads.M_YELLOW, 1, GUIContainer.EMPTY_NAME), 1);
+
+        AnimatedGUIItem item4 = new AnimatedGUIItem(null, false, 13);
+        item4.addFrame(ItemCreator.createHeadItem(Base64Heads.WHITE, 1, GUIContainer.EMPTY_NAME), 5);
+        item4.addFrame(-1, 0, MovementType.OVERRIDE_ITEM, true, 5);
+        item4.addFrame(ItemCreator.createHeadItem(Base64Heads.P_WHITE, 1, GUIContainer.EMPTY_NAME), 5);
+        item4.addFrame(ItemCreator.createHeadItem(Base64Heads.P_RED, 1, GUIContainer.EMPTY_NAME), 1);
+        item4.addFrame(ItemCreator.createHeadItem(Base64Heads.P_ORANGE, 1, GUIContainer.EMPTY_NAME), 1);
+        item4.addFrame(ItemCreator.createHeadItem(Base64Heads.P_YELLOW, 1, GUIContainer.EMPTY_NAME), 1);
+
+        AnimatedGUIItem item5 = new AnimatedGUIItem(null, false, 14);
+        item5.addFrame(ItemCreator.createHeadItem(Base64Heads.WHITE, 1, GUIContainer.EMPTY_NAME), 5);
+        item5.addFrame(-1, 0, MovementType.OVERRIDE_ITEM, true, 5);
+        item5.addFrame(ItemCreator.createHeadItem(Base64Heads.L_WHITE, 1, GUIContainer.EMPTY_NAME), 5);
+        item5.addFrame(ItemCreator.createHeadItem(Base64Heads.L_RED, 1, GUIContainer.EMPTY_NAME), 1);
+        item5.addFrame(ItemCreator.createHeadItem(Base64Heads.L_ORANGE, 1, GUIContainer.EMPTY_NAME), 1);
+        item5.addFrame(ItemCreator.createHeadItem(Base64Heads.L_YELLOW, 1, GUIContainer.EMPTY_NAME), 1);
+
+        AnimatedGUIItem item6 = new AnimatedGUIItem(null, false, 15);
+        item6.addFrame(ItemCreator.createHeadItem(Base64Heads.WHITE, 1, GUIContainer.EMPTY_NAME), 5);
+        item6.addFrame(-1, 0, MovementType.OVERRIDE_ITEM, true, 5);
+        item6.addFrame(ItemCreator.createHeadItem(Base64Heads.E_WHITE, 1, GUIContainer.EMPTY_NAME), 5);
+        item6.addFrame(ItemCreator.createHeadItem(Base64Heads.E_RED, 1, GUIContainer.EMPTY_NAME), 1);
+        item6.addFrame(ItemCreator.createHeadItem(Base64Heads.E_ORANGE, 1, GUIContainer.EMPTY_NAME), 1);
+        item6.addFrame(ItemCreator.createHeadItem(Base64Heads.E_YELLOW, 1, GUIContainer.EMPTY_NAME), 1);
+
+
+        AnimatedGUIItem item7 = new AnimatedGUIItem(null, false, 20);
+        item7.addFrame(ItemCreator.createHeadItem(Base64Heads.WHITE, 1, GUIContainer.EMPTY_NAME), 5);
+        item7.addFrame(-1, 0, MovementType.OVERRIDE_ITEM, true, 5);
+        item7.addFrame(ItemCreator.createHeadItem(Base64Heads.S_WHITE, 1, GUIContainer.EMPTY_NAME), 5);
+        item7.addFrame(ItemCreator.createHeadItem(Base64Heads.S_BLACK, 1, GUIContainer.EMPTY_NAME), 1);
+        item7.addFrame(ItemCreator.createHeadItem(Base64Heads.S_RED, 1, GUIContainer.EMPTY_NAME), 1);
+        item7.addFrame(ItemCreator.createHeadItem(Base64Heads.S_ORANGE, 1, GUIContainer.EMPTY_NAME), 1);
+
+        AnimatedGUIItem item8 = new AnimatedGUIItem(null, false, 19);
+        item8.addFrame(ItemCreator.createHeadItem(Base64Heads.WHITE, 1, GUIContainer.EMPTY_NAME), 5);
+        item8.addFrame(-1, 0, MovementType.OVERRIDE_ITEM, true, 5);
+        item8.addFrame(ItemCreator.createHeadItem(Base64Heads.T_WHITE, 1, GUIContainer.EMPTY_NAME), 5);
+        item8.addFrame(ItemCreator.createHeadItem(Base64Heads.T_BLACK, 1, GUIContainer.EMPTY_NAME), 1);
+        item8.addFrame(ItemCreator.createHeadItem(Base64Heads.T_RED, 1, GUIContainer.EMPTY_NAME), 1);
+        item8.addFrame(ItemCreator.createHeadItem(Base64Heads.T_ORANGE, 1, GUIContainer.EMPTY_NAME), 1);
+
+        AnimatedGUIItem item9 = new AnimatedGUIItem(null, false, 18);
+        item9.addFrame(ItemCreator.createHeadItem(Base64Heads.WHITE, 1, GUIContainer.EMPTY_NAME), 5);
+        item9.addFrame(-1, 0, MovementType.OVERRIDE_ITEM, true, 5);
+        item9.addFrame(ItemCreator.createHeadItem(Base64Heads.A_WHITE, 1, GUIContainer.EMPTY_NAME), 5);
+        item9.addFrame(ItemCreator.createHeadItem(Base64Heads.A_BLACK, 1, GUIContainer.EMPTY_NAME), 1);
+        item9.addFrame(ItemCreator.createHeadItem(Base64Heads.A_RED, 1, GUIContainer.EMPTY_NAME), 1);
+        item9.addFrame(ItemCreator.createHeadItem(Base64Heads.A_ORANGE, 1, GUIContainer.EMPTY_NAME), 1);
+
+        AnimatedGUIItem item10 = new AnimatedGUIItem(null, false, 17);
+        item10.addFrame(ItemCreator.createHeadItem(Base64Heads.WHITE, 1, GUIContainer.EMPTY_NAME), 5);
+        item10.addFrame(-1, 0, MovementType.OVERRIDE_ITEM, true, 5);
+        item10.addFrame(ItemCreator.createHeadItem(Base64Heads.C_WHITE, 1, GUIContainer.EMPTY_NAME), 5);
+        item10.addFrame(ItemCreator.createHeadItem(Base64Heads.C_BLACK, 1, GUIContainer.EMPTY_NAME), 1);
+        item10.addFrame(ItemCreator.createHeadItem(Base64Heads.C_RED, 1, GUIContainer.EMPTY_NAME), 1);
+        item10.addFrame(ItemCreator.createHeadItem(Base64Heads.C_ORANGE, 1, GUIContainer.EMPTY_NAME), 1);
+
+        AnimatedGUIItem item11 = new AnimatedGUIItem(null, false, 16);
+        item11.addFrame(ItemCreator.createHeadItem(Base64Heads.WHITE, 1, GUIContainer.EMPTY_NAME), 5);
+        item11.addFrame(-1, 0, MovementType.OVERRIDE_ITEM, true, 5);
+        item11.addFrame(ItemCreator.createHeadItem(Base64Heads.K_WHITE, 1, GUIContainer.EMPTY_NAME), 5);
+        item11.addFrame(ItemCreator.createHeadItem(Base64Heads.K_BLACK, 1, GUIContainer.EMPTY_NAME), 1);
+        item11.addFrame(ItemCreator.createHeadItem(Base64Heads.K_RED, 1, GUIContainer.EMPTY_NAME), 1);
+        item11.addFrame(ItemCreator.createHeadItem(Base64Heads.K_ORANGE, 1, GUIContainer.EMPTY_NAME), 1);
+
+        textLayer.setItem(3, 2, item1);
+        textLayer.setItem(3, 3, item2);
+        textLayer.setItem(3, 4, item3);
+        textLayer.setItem(3, 5, item4);
+        textLayer.setItem(3, 6, item5);
+        textLayer.setItem(3, 7, item6);
+
+        textLayer.setItem(4, 4, item7);
+        textLayer.setItem(4, 5, item8);
+        textLayer.setItem(4, 6, item9);
+        textLayer.setItem(4, 7, item10);
+        textLayer.setItem(4, 8, item11);
     }
 
     private void genFly(GUILayer flyLayer)
@@ -88,8 +319,28 @@ public class GUIAboutModule extends GUIModule
     private AnimatedGUIItem getFlyItem(int column)
     {
         Random random = new Random();
-        AnimatedGUIItem item = new AnimatedGUIItem(null, true, random.nextInt(50) + 10);
-        ItemStack stack = ItemCreator.createHeadItem(Base64Heads.WHITE, 1, GUIContainer.EMPTY_NAME);
+        AnimatedGUIItem item = new AnimatedGUIItem(null, true, random.nextInt(50) + 50);
+        String str = null;
+        int randNum = random.nextInt(4);
+        switch(randNum)
+        {
+            case 0:
+                str = Base64Heads.BLUE;
+                break;
+            case 1:
+                str = Base64Heads.PURPLE;
+                break;
+            case 2:
+                str = Base64Heads.PINK;
+                break;
+            case 3:
+                str = Base64Heads.MAGENTA;
+                break;
+            case 4:
+                str = Base64Heads.LIGHT_BLUE;
+                break;
+        }
+        ItemStack stack = ItemCreator.createHeadItem(str, 1, GUIContainer.EMPTY_NAME);
         item.addFrame(stack, 1, column, MovementType.OVERRIDE_ITEM, false, 1);
         for(int row = 2; row <= 6; ++row)
         {
