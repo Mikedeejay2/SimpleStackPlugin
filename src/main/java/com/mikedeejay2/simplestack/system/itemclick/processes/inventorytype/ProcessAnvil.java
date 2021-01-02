83a15514eb0acbe44d36547771350e682c850ac1
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-01-02 15:46:17 -0500

    Work on anvil processing

--- src/main/java/com/mikedeejay2/simplestack/system/itemclick/processes/inventorytype/ProcessAnvil.java
+++ src/main/java/com/mikedeejay2/simplestack/system/itemclick/processes/inventorytype/ProcessAnvil.java
@@ -2,6 +2,10 @@ package com.mikedeejay2.simplestack.system.itemclick.processes.inventorytype;
 
 import com.mikedeejay2.simplestack.system.itemclick.ItemClickInfo;
 import com.mikedeejay2.simplestack.system.itemclick.processes.ItemClickProcess;
+import org.bukkit.Sound;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.inventory.ItemStack;
 
 public class ProcessAnvil implements ItemClickProcess
 {
@@ -9,6 +13,37 @@ public class ProcessAnvil implements ItemClickProcess
     public void invoke(ItemClickInfo info)
     {
         if(info.rawSlot != 2) return;
+        switch(info.getAction())
+        {
+            case PICKUP_ALL:
+            case PICKUP_HALF:
+            case PICKUP_ONE:
+            case PICKUP_SOME:
+            case MOVE_TO_OTHER_INVENTORY:
+                break;
+            default:
+                return;
+        }
         System.out.println("Process Anvil");
+        ItemStack item1  = info.topInv.getItem(0);
+        ItemStack         item2  = info.topInv.getItem(1);
+        ItemStack         result = info.topInv.getItem(2);
+        if(result != null)
+        {
+            if(item2 != null)
+            {
+                if(item2.getAmount() > item1.getAmount())
+                {
+                    result.setAmount(item1.getAmount());
+                }
+                else
+                {
+                    result.setAmount(item2.getAmount());
+                }
+                item2.setAmount(item2.getAmount() - (int) Math.ceil(result.getAmount()));
+            }
+            if(item1 != null) item1.setAmount(item1.getAmount() - (int) Math.ceil(result.getAmount()));
+            info.player.getWorld().playSound(info.player.getLocation(), Sound.BLOCK_ANVIL_USE, 1, 1);
+        }
     }
 }
