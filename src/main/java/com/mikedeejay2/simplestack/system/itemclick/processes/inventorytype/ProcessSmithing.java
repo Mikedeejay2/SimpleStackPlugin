5cd83fbf94144af6e40acc77fe400d84cdb01b2d
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-02-03 20:07:18 -0500

    Add ProcessSmithing full functionality

--- src/main/java/com/mikedeejay2/simplestack/system/itemclick/processes/inventorytype/ProcessSmithing.java
+++ src/main/java/com/mikedeejay2/simplestack/system/itemclick/processes/inventorytype/ProcessSmithing.java
@@ -3,6 +3,7 @@ package com.mikedeejay2.simplestack.system.itemclick.processes.inventorytype;
 import com.mikedeejay2.simplestack.system.itemclick.ItemClickInfo;
 import com.mikedeejay2.simplestack.system.itemclick.processes.ItemClickProcess;
 import org.bukkit.Sound;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public class ProcessSmithing implements ItemClickProcess
@@ -10,6 +11,7 @@ public class ProcessSmithing implements ItemClickProcess
     @Override
     public void invoke(ItemClickInfo info)
     {
+        Inventory inventory = info.topInv;
         if(info.rawSlot != 2) return;
         switch(info.getAction())
         {
@@ -23,25 +25,34 @@ public class ProcessSmithing implements ItemClickProcess
                 return;
         }
         /* DEBUG */ System.out.println("Process Smithing");
-        ItemStack item1  = info.topInv.getItem(0);
-        ItemStack         item2  = info.topInv.getItem(1);
-        ItemStack         result = info.topInv.getItem(2);
-        if(result != null)
+        ItemStack result = inventory.getItem(2);
+        if(result == null) return;
+
+        ItemStack item1 = inventory.getItem(0);
+        ItemStack item2 = inventory.getItem(1);
+        boolean item1Null = item1 == null;
+        boolean item2Null = item2 == null;
+        int item1Amount = item1Null ? 0 : item1.getAmount();
+        int item2Amount = item2Null ? 0 : item2.getAmount();
+        int amount = 1;
+        if(!item1Null && !item2Null)
         {
-            if(item2 != null)
-            {
-                if(item2.getAmount() > item1.getAmount())
-                {
-                    result.setAmount(item1.getAmount());
-                }
-                else
-                {
-                    result.setAmount(item2.getAmount());
-                }
-                item2.setAmount(item2.getAmount() - (int) Math.ceil(result.getAmount()));
-            }
-            if(item1 != null) item1.setAmount(item1.getAmount() - (int) Math.ceil(result.getAmount()));
-            info.player.getWorld().playSound(info.player.getLocation(), Sound.BLOCK_SMITHING_TABLE_USE, 1, 1);
+            amount = Math.min(item1Amount, item2Amount);
         }
+        else if(!item1Null)
+        {
+            amount = item1Amount;
+        }
+
+        if(!item1Null)
+        {
+            item1.setAmount(item1Amount - amount);
+        }
+        if(!item2Null)
+        {
+            item2.setAmount(item2Amount - amount);
+        }
+
+        info.player.getWorld().playSound(info.player.getLocation(), Sound.BLOCK_SMITHING_TABLE_USE, 1, 1);
     }
 }
