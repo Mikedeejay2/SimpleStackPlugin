63b9a46e3261071aecfde47ec7e3d00400a64cf0
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-04-10 16:05:13 -0400

    Add FunctionalInterface where needed

--- src/main/java/com/mikedeejay2/simplestack/system/SimpleStackHandler.java
+++ src/main/java/com/mikedeejay2/simplestack/system/SimpleStackHandler.java
@@ -2,6 +2,7 @@ package com.mikedeejay2.simplestack.system;
 
 import org.bukkit.event.Event;
 
+@FunctionalInterface
 public interface SimpleStackHandler<E extends Event>
 {
     void handle(E event);
--- src/main/java/com/mikedeejay2/simplestack/system/itemclick/processes/ItemClickProcess.java
+++ src/main/java/com/mikedeejay2/simplestack/system/itemclick/processes/ItemClickProcess.java
@@ -3,6 +3,7 @@ package com.mikedeejay2.simplestack.system.itemclick.processes;
 import com.mikedeejay2.simplestack.system.SimpleStackProcess;
 import com.mikedeejay2.simplestack.system.itemclick.ItemClickInfo;
 
+@FunctionalInterface
 public interface ItemClickProcess extends SimpleStackProcess
 {
     void invoke(ItemClickInfo info);
