2f7123ecb6b65059fd6daaa181360ad42d74ed16
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2022-08-01 16:33:35 -0400

    Add Javadocs for Byte Buddy classes

--- src/main/java/com/mikedeejay2/simplestack/bytebuddy/ByteBuddyHolder.java
+++ src/main/java/com/mikedeejay2/simplestack/bytebuddy/ByteBuddyHolder.java
@@ -3,9 +3,23 @@ package com.mikedeejay2.simplestack.bytebuddy;
 import net.bytebuddy.agent.ByteBuddyAgent;
 import java.lang.instrument.Instrumentation;
 
+/**
+ * Simple holder class for initializing the Byte Buddy agent and storing its instrumentation instance.
+ *
+ * @author Mikedeejay2
+ * @since 2.0.0
+ */
 public class ByteBuddyHolder {
+    /**
+     * The stored {@link Instrumentation} of the Byte Buddy agent
+     */
     private static Instrumentation instrumentation;
 
+    /**
+     * Install the {@link ByteBuddyAgent}
+     *
+     * @return Whether a failure has occurred
+     */
     public static boolean initialize() {
         try {
             instrumentation = ByteBuddyAgent.install();
@@ -15,6 +29,11 @@ public class ByteBuddyHolder {
         return false;
     }
 
+    /**
+     * Get the Byte Buddy agent {@link Instrumentation} instance.
+     *
+     * @return The Byte Buddy agent {@link Instrumentation}
+     */
     public static Instrumentation getInstrumentation() {
         return instrumentation;
     }
