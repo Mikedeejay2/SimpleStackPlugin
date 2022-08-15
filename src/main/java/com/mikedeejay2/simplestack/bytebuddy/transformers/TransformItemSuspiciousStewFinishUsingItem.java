8c3e9456b6a1fb88f34a393bb9d91443a666e92e
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2022-08-15 00:51:43 -0400

    Clean up TransformItemSuspiciousStewFinishUsingItem

--- src/main/java/com/mikedeejay2/simplestack/bytebuddy/transformers/TransformItemSuspiciousStewFinishUsingItem.java
+++ src/main/java/com/mikedeejay2/simplestack/bytebuddy/transformers/TransformItemSuspiciousStewFinishUsingItem.java
@@ -1,8 +1,6 @@
 package com.mikedeejay2.simplestack.bytebuddy.transformers;
 
 import com.mikedeejay2.simplestack.MappingsLookup;
-import com.mikedeejay2.simplestack.bytebuddy.SimpleStackMethodVisitor;
-import net.bytebuddy.jar.asm.Label;
 
 import static com.mikedeejay2.simplestack.MappingsLookup.*;
 import static net.bytebuddy.jar.asm.Opcodes.*;
@@ -17,7 +15,6 @@ public class TransformItemSuspiciousStewFinishUsingItem extends TransformItemSou
     public void visitFrame(int type, int numLocal, Object[] local, int numStack, Object[] stack) {
         if(!visitedFrame && visitedReturn) { // Target the frame after the first return statement
             visitedFrame = true;
-            System.out.println("Visited frame");
             // Instead of F_APPEND, F_SAME is instead used for suspicious stew.
             super.visitFrame(F_SAME, 0, null, 0, null);
             return;
